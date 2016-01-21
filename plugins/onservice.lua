462512423a183af9387904313c8a39c16202cf63
Creedsteam <0.creed.is.dead.0@gmail.com>
2016-01-21 11:35:38 +0330

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -2,7 +2,7 @@ do
 -- Will leave the group if be added
 local function run(msg, matches)
 local bot_id = our_id -- your bot id
-   -- like local bot_id = 1234567
+   -- like local bot_id = 167946378
     if matches[1] == 'leave' and is_admin(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
