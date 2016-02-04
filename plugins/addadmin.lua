62f1f8f66ba17a209f8b3709b98d63db2b5cdb4f
Creed is DeaD <0.creed.is.dead.0@gmail.com>
2016-02-04 15:09:16 +0330

    Create addadmin.lua

---
+++ plugins/addadmin.lua
@@ -0,0 +1,21 @@
+do
+local function callback(extra, success, result)
+    vardump(success)
+    cardump(result)
+end
+    function run(msg, matches)
+        if not is_momod or not is_owner then
+    return "Only Onwers Can Add WilSoN!"
+end
+    local user = 'user#id'
+    local chat = 'chat#id'..msg.to.id
+    chat_add_user(chat, user, callback, false)
+    return "Admin Added To: "..string.gsub(msg.to.print_name, "_", " ")..'['..msg.to.id..']'
+end
+return {
+    patterns ={
+        "^([Aa]ddadmin)$"
+        },
+    run = run
+}
+end
