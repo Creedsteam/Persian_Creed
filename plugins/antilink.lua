2317df4c14b4be3fe09efda150876acbbb44e8be
Creed is DeaD <0.creed.is.dead.0@gmail.com>
2016-02-05 11:35:12 +0330

    Update antilink.lua

--- plugins/antilink.lua
+++ plugins/antilink.lua
@@ -5,14 +5,14 @@ local function run(msg)
     
     local data = load_data(_config.moderation.data)
     
-     if data[tostring(msg.to.id)]['settings']['antilink'] == 'yes' then
+     if data[tostring(msg.to.id)]['settings']['lock_adds'] == 'yes' then
       
     
 if not is_momod(msg) then
     
     
 chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-    local msgads = 'ForbiddenAdText'
+    local msgads = 'You cannot send link here !'
    local receiver = msg.to.id
     send_large_msg('chat#id'..receiver, msg.."\n", ok_cb, false)
 	
