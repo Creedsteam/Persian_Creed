876adeee5623d42d42a54f67c711c0435b1e9f92
Creed is DeaD <0.creed.is.dead.0@gmail.com>
2016-02-25 12:33:58 +0330

    Update antitag.lua

--- plugins/antitag.lua
+++ plugins/antitag.lua
@@ -9,7 +9,7 @@ if not is_momod(msg) then
 
 
 chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-    local msgtag = 'You cant tag anything here '
+    local msgtag = 'تگ کردن در این گروه غیر مجاز است بنابر این اخراج میشوید '
    local receiver = msg.to.id
     send_large_msg('chat#id'..receiver, msgads.."\n", ok_cb, false)
 
