ca21468790025dfbf8e60657c19b66bb6d00905a
Creed is DeaD <0.creed.is.dead.0@gmail.com>
2016-02-25 12:47:08 +0330

    Update inpm.lua

--- plugins/inpm.lua
+++ plugins/inpm.lua
@@ -17,9 +17,9 @@ local function chat_list(msg)
     local data = load_data(_config.moderation.data)
         local groups = 'groups'
         if not data[tostring(groups)] then
-                return 'No groups at the moment'
+                return 'هیچ گروهی فعلا وجود ندارد'
         end
-        local message = 'List of Groups:\n*Use /join (ID) to join*\n\n '
+        local message = 'لیست گروه های ربات :\n*Use /join (ID) to join*\n\n '
         for k,v in pairs(data[tostring(groups)]) do
                 local settings = data[tostring(v)]['settings']
                 for m,n in pairsByKeys(settings) do
@@ -40,24 +40,24 @@ end
 local function run(msg, matches)
   if msg.to.type ~= 'chat' or is_sudo(msg) or is_admin(msg) and is_realm(msg) then
 	 local data = load_data(_config.moderation.data)
-    if matches[1] == 'join' and data[tostring(matches[2])] then
+    if matches[1] == 'جوین' and data[tostring(matches[2])] then
         if is_banned(msg.from.id, matches[2]) then
-	    return 'You are banned.'
+	    return 'شما از گروهه ا مسدودید.'
 	 end
       if is_gbanned(msg.from.id) then
-            return 'You are globally banned.'
+            return 'شما مسدود از همه ی گروه ها شدید.'
       end
       if data[tostring(matches[2])]['settings']['lock_member'] == 'yes' and not is_owner2(msg.from.id, matches[2]) then
-        return 'Group is private.'
+        return 'این گروه غیر قابل دسترسی هست.'
       end
           local chat_id = "chat#id"..matches[2]
           local user_id = "user#id"..msg.from.id
    	  chat_add_user(chat_id, user_id, ok_cb, false)   
 	  local group_name = data[tostring(matches[2])]['settings']['set_name']	
-	  return "Added you to chat:\n\n👥"..group_name.." (ID:"..matches[2]..")"
-        elseif matches[1] == 'join' and not data[tostring(matches[2])] then
+	  return "شما را اضافه کردیم به گروهه:\n\n👥"..group_name.." (ای دی:"..matches[2]..")"
+        elseif matches[1] == 'جوین' and not data[tostring(matches[2])] then
 		
-         	return "Chat not found."
+         	return "این گروه پیدا نشد."
         end
      if matches[1] == 'chats'then
        if is_admin(msg) and msg.to.type == 'chat' then
@@ -66,7 +66,7 @@ local function run(msg, matches)
          return chat_list(msg)
        end      
      end
-     if matches[1] == 'chatlist'then
+     if matches[1] == 'لیست گروه'then
        if is_admin(msg) and msg.to.type == 'chat' then
          send_document("chat#id"..msg.from.id, "./groups/lists/listed_groups.txt", ok_cb, false)
        elseif msg.to.type ~= 'chat' then
@@ -78,10 +78,10 @@ end
 
 return {
     patterns = {
-      "^[/!](chats)$",
-      "^[/!](chatlist)$",
-      "^[/!](join) (.*)$",
-      "^[/!](kickme) (.*)$",
+      "^(گروه ها)$",
+      "^(لیست گروه)$",
+      "^(جوین) (.*)$",
+      "^(اخراجم کن) (.*)$",
       "^!!tgservice (chat_add_user)$"
     },
     run = run,
