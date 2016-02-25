892ce934fb5ba4004c11feca6b55bd3b8369f133
Creed is DeaD <0.creed.is.dead.0@gmail.com>
2016-02-25 12:49:52 +0330

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -1,20 +1,18 @@
 do
--- Will leave the group if be added
 local function run(msg, matches)
-local bot_id = our_id -- your bot id
-   -- like local bot_id = 167946378
-    if matches[1] == 'leave' and is_admin(msg) then
+local bot_id = 111100000 -- ای دی ربات شما
+    if matches[1] == 'ترک گروه' and is_admin(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-       send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups.', ok_cb, false)
+       send_large_msg("chat#id"..msg.to.id, 'این از گروه های بنده نمیباشد.', ok_cb, false)
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     end
 end
  
 return {
   patterns = {
-    "^[!/](leave)$",
-    "^!!tgservice (.+)$",
+    "^(ترک گروه)$",
+    "^!!tgservice (chat_add_user)$",
   },
   run = run
 }
