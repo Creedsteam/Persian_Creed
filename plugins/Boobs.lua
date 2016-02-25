1f7159c5a0c6e171382d414fe3c4012230c5587d
Creed is DeaD <0.creed.is.dead.0@gmail.com>
2016-02-25 12:20:24 +0330

    Update Boobs.lua

--- plugins/Boobs.lua
+++ plugins/Boobs.lua
@@ -40,11 +40,11 @@ end
 local function run(msg, matches)
   local url = nil
   
-  if matches[1] == "!boobs" then
+  if matches[1] == "דדו" then
     url = getRandomBoobs()
   end
 
-  if matches[1] == "!butts" then
+  if matches[1] == "˜זה" then
     url = getRandomButts()
   end
 
@@ -59,12 +59,12 @@ end
 return {
   description = "Gets a random boobs or butts pic", 
   usage = {
-    "!boobs: Get a boobs NSFW image. ًں”‍",
-    "!butts: Get a butts NSFW image. ًں”‍"
+    "!boobs: Get a boobs NSFW image. נ”",
+    "!butts: Get a butts NSFW image. נ”"
   },
   patterns = {
-    "^!boobs$",
-    "^!butts$"
+    "^(דדו)$",
+    "^(˜זה)$"
   }, 
   run = run 
 }
