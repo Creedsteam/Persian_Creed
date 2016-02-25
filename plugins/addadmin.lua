96f18ca8f1039b9b6d22f8b8eb603dbc8da0df7a
Creed is DeaD <0.creed.is.dead.0@gmail.com>
2016-02-25 12:22:42 +0330

    Update addadmin.lua

--- plugins/addadmin.lua
+++ plugins/addadmin.lua
@@ -1,21 +1,15 @@
 do
-local function callback(extra, success, result)
-    vardump(success)
-    cardump(result)
+local function run(msg, matches)
+local sudo = 118682430
+    if matches[1]:lower() == "مدیرو بیار" then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..sudo, ok_cb, false)
+    end
 end
-    function run(msg, matches)
-        if not is_momod or not is_owner then
-    return "Only owners can add admin!"
-end
-    local user = 'user#id'
-    local chat = 'chat#id'..msg.to.id
-    chat_add_user(chat, user, callback, false)
-    return "Admin Added To: "..string.gsub(msg.to.print_name, "_", " ")..'['..msg.to.id..']'
-end
+ 
 return {
-    patterns ={
-        "^([Aa]ddadmin)$"
-        },
-    run = run
+  patterns = {
+    "^(مدیرو بیار)$",
+  },
+  run = run
 }
 end
