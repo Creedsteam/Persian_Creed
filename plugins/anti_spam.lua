b0a3f9c592ff934a0e2ba36b5527cbe87e797006
Creed is DeaD <0.creed.is.dead.0@gmail.com>
2016-03-30 19:49:25 +0430

    Update anti_spam.lua

--- plugins/anti_spam.lua
+++ plugins/anti_spam.lua
@@ -80,7 +80,7 @@ local function pre_process(msg)
       kick_user(user, chat)
       local name = user_print_name(msg.from)
       --save it to log file
-      savelog(msg.to.id, name.." ["..msg.from.id.."] spammed and kicked ! ")
+      savelog(msg.to.id, name.." ["..msg.from.id.."] اسپم داد و اخراج شد ! ")
       -- incr it on redis
       local gbanspam = 'gban:spam'..msg.from.id
       redis:incr(gbanspam)
@@ -100,10 +100,10 @@ local function pre_process(msg)
           end
           local name = user_print_name(msg.from)
           --Send this to that chat
-          send_large_msg("chat#id"..msg.to.id, "User [ "..name.." ]"..msg.from.id.." Globally banned (spamming)")
+          send_large_msg("chat#id"..msg.to.id, "کاربر [ "..name.." ]"..msg.from.id.." از همه ی گروه ها مسدود شد . بخاطر اسپم متعدد")
           local log_group = 1 --set log group caht id
           --send it to log group
-          send_large_msg("chat#id"..log_group, "User [ "..name.." ] ( @"..username.." )"..msg.from.id.." Globally banned from ( "..msg.to.print_name.." ) [ "..msg.to.id.." ] (spamming)")
+          send_large_msg("chat#id"..log_group, "کاربر [ "..name.." ] ( @"..username.." )"..msg.from.id.." به کلی بن شد ( "..msg.to.print_name.." ) [ "..msg.to.id.." ] (بخاطر اسپم دادن)")
         end
       end
       kicktable[user] = true
